5b5b805f2b0f0929f8f5749131d54cc19e374d93
ytkkk <m17927@naver.com>
2024-08-08 16:41:44 +0900

    Add files via upload

---
+++ ymlmaker.py
@@ -0,0 +1,33 @@
+from fairchem.core.common.tutorial_utils import generate_yml_config
+from fairchem.core.models.model_registry import model_name_to_local_file
+
+checkpoint = model_name_to_local_file('GemNet-OC-S2EFS-OC20+OC22', local_cache='./')
+
+yml = generate_yml_config(checkpoint, 'config.yml',
+                delete=['slurm', 'cmd', 'logger', 'task', 'model_attributes',
+                        'optim.loss_force',
+                        'optim.load_balancing',
+                        'dataset', 'test_dataset', 'val_dataset'],
+                update={'gpus': 1,
+                        'task.dataset': 'lmdb',
+                        'optim.eval_every': 10,
+                        'optim.max_epochs': 1,
+                        'optim.batch_size': 4,
+                        'logger': 'tensorboard',
+                        # Train data
+                        'dataset.train.src': './data/hi_train.lmdb',
+                        'dataset.train.format': 'lmdb',
+                        'dataset.train.a2g_args.r_energy': True,
+                        'dataset.train.a2g_args.r_forces': True,
+                        # Test data - prediction only so no regression
+                        'dataset.test.src': './data/hi_test.lmdb',
+                        'dataset.test.format': 'lmdb',
+                        'dataset.test.a2g_args.r_energy': False,
+                        'dataset.test.a2g_args.r_forces': False,
+                        # val data
+                        'dataset.val.src': './data/hi_val.lmdb',
+                        'dataset.val.format': 'lmdb',
+                        'dataset.val.a2g_args.r_energy': True,
+                        'dataset.val.a2g_args.r_forces': True,
+                        })
+
